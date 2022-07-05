b4a243a3628fe5c16810ec5645ea60cb9911a102
Tony Miller <mcfiredrill@gmail.com>
2022-07-05 10:36:13 +0900

    parse string to int for port

--- lib/fruitbot/supervisor.ex
+++ lib/fruitbot/supervisor.ex
@@ -5,11 +5,21 @@ defmodule Fruitbot.Supervisor do
     Supervisor.start_link(__MODULE__, :ok, opts)
   end
 
+  defp get_port() do
+    port_env_variable = System.get_env("PORT")
+    if is_nil(port_env_variable) do
+      4000
+    else
+      String.to_integer(port_env_variable)
+    end
+  end
+
+
   @impl true
   def init(:ok) do
     children = [
       #{PhoenixClient.Socket, {socket_opts, name: PhoenixClient.Socket}},
-      Plug.Cowboy.child_spec(scheme: :http, plug: Fruitbot.Router, options: [port: System.get_env("PORT") || 4000]),
+      Plug.Cowboy.child_spec(scheme: :http, plug: Fruitbot.Router, options: [port: get_port()]),
       {Fruitbot.Worker, name: Fruitbot.Worker},
       {Fruitbot.NostrumConsumer, name: Fruitbot.NostrumConsumer}
     ]
