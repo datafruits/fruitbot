e0a58d54c8f370b1db88af7b23171eefa146d42c
Tony Miller <mcfiredrill@gmail.com>
2023-03-02 15:06:56 -0800

    add !vr and !donate

--- lib/fruitbot/worker.ex
+++ lib/fruitbot/worker.ex
@@ -64,6 +64,12 @@ defmodule Fruitbot.Worker do
     if Map.has_key?(payload, "body") do
       IO.puts "payload body: #{payload["body"]}"
       case payload["body"] do
+        "!vr" ->
+          msg = "Join the party in VR here! https://hubs.mozilla.com/MsvfAkH/terrific-satisfied-area"
+          send_message(state.channel, msg)
+        "!donate" ->
+          msg = "Enjoying the stream? The best way to support is with a monthly donation on ampled. https://www.ampled.com/artist/datafruits You can also make a one time donation to our paypal. https://paypal.me/datafruitsfm"
+          send_message(state.channel, msg)
         "!advice" ->
           advice = "Don't live like me Brendon. Don't get a tattoo of a cheese cow."
           send_message(state.channel, advice)
