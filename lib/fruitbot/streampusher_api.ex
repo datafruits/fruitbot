94e32af645ea9c8292a28f913a4184bd37a8dae8
Tony Miller <mcfiredrill@gmail.com>
2023-03-20 19:12:00 -0700

    wiki command

--- lib/fruitbot/streampusher_api.ex
+++ lib/fruitbot/streampusher_api.ex
@@ -6,6 +6,13 @@ defmodule Fruitbot.StreampusherApi do
     decoded["scheduled_show"]["tweet_content"]
   end
 
+  def search_wiki(query) do
+    {:ok, resp} = :httpc.request(:get, {'https://datafruits.streampusher.com/api/wiki.json', []}, [], [body_format: :json])
+    json = elem(resp, 2)
+    {:ok, decoded } = Jason.decode(json)
+    decoded["wiki_pages"]["link"]
+  end
+
   # def today do
   #
   # end
