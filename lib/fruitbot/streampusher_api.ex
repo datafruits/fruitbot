12e85c9c5a2cc2a9018ddefa44cc5e007bcfacfa
Tony Miller <mcfiredrill@gmail.com>
2023-03-24 14:10:42 -0700

    show link to !next comand

--- lib/fruitbot/streampusher_api.ex
+++ lib/fruitbot/streampusher_api.ex
@@ -1,4 +1,18 @@
 defmodule Fruitbot.StreampusherApi do
+  def user_search(query) do
+    response =
+      case HTTPoison.get!("https://datafruits.streampusher.com/api/djs/#{query}.json?name=#{query}") do
+        %HTTPoison.Response{status_code: 200, body: body} ->
+          Jason.decode!(body)
+        %HTTPoison.Response{status_code: 404} ->
+          IO.puts("Not found")
+        %HTTPoison.Error{reason: reason} ->
+          IO.inspect(reason)
+      end
+
+      data = response["data"]
+  end
+
   def next_show do
     response =
       case HTTPoison.get!("https://datafruits.streampusher.com/scheduled_shows/next.json") do
@@ -14,7 +28,9 @@ defmodule Fruitbot.StreampusherApi do
       title = Kernel.get_in(data, ["attributes", "title"])
       host = Kernel.get_in(data, ["attributes", "hosted_by"])
       description = Kernel.get_in(data, ["attributes", "description"])
-      "Next show is #{title}, hosted by #{host}! Description: #{description}."
+      slug = Kernel.get_in(data, ["attributes", "slug"])
+      url = "https://datafruits.fm/shows/#{slug}"
+      "Next show is #{title}, hosted by #{host}! Description: #{description}. :link: #{url}"
   end
 
   # def search_wiki(query) do
