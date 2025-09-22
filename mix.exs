d92c9bbd1a2175170f2ae4e8a86d483f34d6825b
Tony Miller <mcfiredrill@gmail.com>
2025-09-22 13:51:41 -0700

    Revert "remove markov"
    This reverts commit b7427a6a73b40011e3e0cce628da56477530588d.

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Fruitbot.MixProject do
       {:httpoison, "~> 2.0"},
       {:tmi, git: "https://github.com/mcfiredrill/tmi.ex" },
       {:castore, "~> 1.0"},
-      {:persistent_ets, "~> 0.1.0"}
+      {:persistent_ets, "~> 0.1.0"},
+      {:markov, "~> 4.0"}
     ]
   end
 end
